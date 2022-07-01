a325d7ff24c6a84f05b07e3d855cfa7efdda71e4
hasannayeeem <hasannayeeem@gmail.com>
2022-07-01 06:14:24 +0600

    finally

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8000;
@@ -8,8 +8,8 @@ const port = process.env.PORT || 8000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dpull.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const toDoCollection = client.db('tasky').collection('todos');
         // all todos api
@@ -25,8 +25,30 @@ async function run(){
             const result = await toDoCollection.insertOne(newTodo);
             res.send(result)
         });
+        // DELETE 
+        app.delete('/todo/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await toDoCollection.deleteOne(query);
+            res.send(result);
+        });
+        // Update or Modify Task
+        app.put('/todos/:id', async (req, res) =>{
+            const id = req.params.id;
+            const updatedTodo = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true};
+            const updatedDoc = {
+                $set: {
+                    task: updatedTodo.task,
+                }
+            };
+            const result = await toDoCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
     }
-    finally{
+    finally {
 
     }
 }
