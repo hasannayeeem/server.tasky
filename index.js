580710f42957223af2425688287f39241c1a34e2
hasannayeeem <hasannayeeem@gmail.com>
2022-07-04 03:58:08 +0600

    few updates

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8000;
-
 // use middleWare 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +15,7 @@ async function run() {
     try {
         await client.connect();
         const toDoCollection = client.db('tasky').collection('todos');
+        const completedCollection = client.db('tasky').collection('completed');
         // all todos api
         app.get('/todos', async (req, res) => {
             const query = {};
@@ -23,11 +23,24 @@ async function run() {
             const todos = await cursor.toArray();
             res.send(todos);
         });
+        // all todos api
+        app.get('/completed', async (req, res) => {
+            const query = {};
+            const cursor = completedCollection.find(query);
+            const completed = await cursor.toArray();
+            res.send(completed);
+        });
         //POST
         app.post('/todos', async (req, res) => {
             const newTodo = req.body;
             const result = await toDoCollection.insertOne(newTodo);
             res.send(result)
+        });
+        // posting completed task
+        app.post("/completed", async (req, res) => {
+            const completed = req.body;
+            const result = await completedCollection.insertOne(completed);
+            res.send(result);
         });
         // DELETE 
         app.delete('/todo/:id', async (req, res) => {
@@ -37,12 +50,35 @@ async function run() {
             const result = await toDoCollection.deleteOne(query);
             res.send(result);
         });
+        // DELETE for completed task
+        app.delete('/completed/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            console.log(query);
+            const result = await completedCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        app.put("/upToDo/:id", async (req, res) => {
+            const id = req.params.id;
+            const todo = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: {
+                data: todo.updateTodo,
+              }
+            };
+            const Todo = await toDoCollection.updateOne(filter, updateDoc, options);
+            res.send({ success: true, data: Todo });
+          });
         // Update or Modify Task
-        app.put('/todos/:id', async (req, res) =>{
+        app.put('/todos/:id', async (req, res) => {
             const id = req.params.id;
             const updatedTodo = req.body;
-            const filter = {_id: ObjectId(id)};
-            const options = { upsert: true};
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
             const updatedDoc = {
                 $set: {
                     task: updatedTodo.task,
@@ -57,25 +93,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-// use middleWare 
-app.use(cors());
-app.use(express.json());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.get('/', (req, res) => {
